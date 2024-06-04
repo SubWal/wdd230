b06468fcdece6b72dd6c280be3197c28d798a499
SubWal <subodh333@byui.edu>
2024-06-04 00:48:53 -0600

    h1

--- week02/bom.js
+++ week02/bom.js
@@ -1,48 +1,48 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector("button");
-const list = document.querySelector("#list");
+// const input = document.querySelector("#favchap");
+// const button = document.querySelector("button");
+// const list = document.querySelector("#list");
 
-button.addEventListener("click", () => {
-  if (input.value) {
-    //create li
-    const li = document.createElement("li");
+// button.addEventListener("click", () => {
+//   if (input.value) {
+//     //create li
+//     const li = document.createElement("li");
 
-    //create the delete button
+//     //create the delete button
 
-    const deleteButton = document.createElement("button");
+//     const deleteButton = document.createElement("button");
 
-    //pupulate the li element
-    li.textContent = input.value;
+//     //pupulate the li element
+//     li.textContent = input.value;
 
-    //populate the delete button
-    deleteButton.textContent = "❌";
+//     //populate the delete button
+//     deleteButton.textContent = "❌";
 
-    //append the button to the li element
+//     //append the button to the li element
 
-    li.appendChild(deleteButton);
+//     li.appendChild(deleteButton);
 
-    //append the li element to the list
-    list.appendChild(li);
-  }
+//     //append the li element to the list
+//     list.appendChild(li);
+//   }
 
-    //envent to the delete button
+//     //envent to the delete button
 
-    deleteButton.addEventListener("click", () => {
-        // list.removeChild(li);
+//     deleteButton.addEventListener("click", () => {
+//         // list.removeChild(li);
 
-        // clearInput()
+//         // clearInput()
     
 
-    else {
+//     else {
         
         
-        input.focus();
-    }
+//         input.focus();
+//     }
 
-    clearInput()
-    //chosse the input value to nothing or the empty string to clean up the empty list
+//     clearInput()
+//     //chosse the input value to nothing or the empty string to clean up the empty list
     
-    });
+//     });
 
     
 
@@ -50,4 +50,4 @@ button.addEventListener("click", () => {
 
 
 
-  }
+//   }
